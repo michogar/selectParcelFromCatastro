069e20ac0e5a4753f27d8b84d7b79ff208e559c0
michogar <michogar@gmail.com>
2017-04-22 00:16:53 +0200

    First commit
    created node + express project with webstorm
    added spec tools:
    * chai
    * mocha
    
    and coverage tools:
    * nyc

---
+++ test/MapSpec.js
@@ -0,0 +1,11 @@
+/**
+ * Created by michogarcia on 22/04/17.
+ */
+const expect = require('chai').expect
+
+describe("Map Spec: ", () => {
+
+    it("", () => {
+        expect(false).to.equal(true)
+    })
+})
